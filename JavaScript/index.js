65b4e68b126c484b1afd79e5ed812f2aacc3704f
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2023-08-27 20:04:16 +0500

    Some Modifications

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -38,8 +38,7 @@ ScrollReveal({
     delay: 200
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .skill-container, .portfolio-box, .contact form', { origin: 'bottom' });
+
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
